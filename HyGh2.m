4b2f5f7088aba1d5305e55dc4e98ee8518c50e66
black3391 <shuhanqi@gmail.com>
2017-06-29 18:05:49 +0800

    6/29

--- HyGh2.m
+++ HyGh2.m
@@ -8,8 +8,8 @@ function [M] = HyGh2( X, sigma,k )
 %     0 1 2 1 1 1; 
 %     1 0 1 1 0 1];
 H = zeros(size(X,1),size(X,1));
-dist= exp(-sqdist(X)/(2*sigma*sigma));
-%dist = sqdist(X);
+%dist= exp(-sqdist(X)/(2*sigma*sigma));
+dist = sqdist(X);
 for i = 1:size(dist,2)
 t = dist(:,i);
 [pos1,pos2]=sort(t,'descend');
--- demo_SHDH.m
+++ demo_SHDH.m
@@ -38,7 +38,7 @@ anchor = traindata(randsample(Ntrain, n_anchors),:);
 % sigma = mean(min(Dis,[],2).^0.5);
 % clear Dis
 sigma = 0.4; % for normalized data
-sample = randsample(Ntrain, 10000);
+sample = randsample(Ntrain, 3000);
 Train_label = label(sample,:);
 Train_sample= traindata(sample,:);
 [A] = HyGh2( Train_sample,sigma, 7 );
@@ -46,13 +46,18 @@ Train_sample= traindata(sample,:);
 % PhiX = exp(-sqdist(traindata,anchor)/(2*sigma*sigma));
 % PhiX = [PhiX, ones(Ntrain,1)];
 
-Phi_testdata = exp(-sqdist(testdata,anchor)/(2*sigma*sigma)); clear testdata
-Phi_testdata = [Phi_testdata, ones(size(Phi_testdata,1),1)];
-Phi_traindata = exp(-sqdist(traindata,anchor)/(2*sigma*sigma)); clear traindata;
-Phi_traindata = [Phi_traindata, ones(size(Phi_traindata,1),1)];
+Phi_testdata = exp(-sqdist(testdata,anchor)/(2*sigma*sigma)); 
+%clear testdata
+%Phi_testdata = [Phi_testdata, ones(size(Phi_testdata,1),1)];
+Phi_traindata = exp(-sqdist(traindata,anchor)/(2*sigma*sigma)); 
+%clear traindata;
+%Phi_traindata = [Phi_traindata, ones(size(Phi_traindata,1),1)];
 Phi_trainsample = exp(-sqdist(Train_sample,anchor)/(2*sigma*sigma));
-Phi_trainsample = [Phi_trainsample, ones(size(Phi_trainsample,1),1)];
+%Phi_trainsample = [Phi_trainsample, ones(size(Phi_trainsample,1),1)];
 
+Phi_trainsample=Train_sample;
+Phi_traindata=traindata;
+Phi_testdata=testdata;
 
 % learn G and F
 %betaarray=[0,0.0000005,0.000001,0.000005,0.00001,0.00005,0.0001,0.0005,0.001,0.005,0.01];
@@ -60,7 +65,7 @@ betaarray=[0,0.000005,0.00001,0.00005,0.0001,0.0005];
 maxItr = 7;
 gmap.lambda = 1; gmap.loss = 'L2';
 Fmap.type = 'RBF';
-Fmap.nu = 0; %  penalty parm for F term
+Fmap.nu = 1e-5; %  penalty parm for F term
 Fmap.lambda = 1e-2;
 %Hmap.beta = 0.000;
 Hmap.graph=A;
